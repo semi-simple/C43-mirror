8af2c2af061f9d4f11599e17ede3f6e50c639e9f
Martin <github@lorang.net>
2019-01-12 06:24:49 +0100

    Make function sin work with complex numbers

--- src/wp43s/mathematics/sin.c
+++ src/wp43s/mathematics/sin.c
@@ -22,51 +22,190 @@
 
 
 
+void (* const Sin[12])(void) = {
+//  regX ==> 1            2         3          4          5          6          7          8            9             10              11        12
+//           Big integer  real16    complex16             Time       Date       String     real16 mat   complex16 m   Small integer   real34    complex34
+             sinBigI,     sinRe16,  sinCo16,   errorSin,  errorSin,  errorSin,  errorSin,  sinRm16,     sinCm16,      errorSin,       sinRe34,  sinCo34
+};
+
+
+
+/********************************************//**
+ * \brief Data type error in sin
+ *
+ * \param void
+ * \return void
+ ***********************************************/
+void errorSin(void) {
+  displayCalcErrorMessage(24, REGISTER_T, REGISTER_X);
+  #if (EXTRA_INFO_ON_CALC_ERROR == 1)
+    sprintf(errorMessage, "cannot calculate Sin for %s", getRegisterDataTypeName(op1, true, false));
+    showInfoDialog("In function fnSin:", errorMessage, NULL, NULL);
+  #endif
+}
+
+
+
+/********************************************//**
+ * \brief Error message for a valid operation to be coded
+ *
+ * \param void
+ * \return void
+ ***********************************************/
+void sinToBeCoded(void) {
+  #ifdef PC_BUILD
+    sprintf(errorMessage, "sin(%s)", getRegisterDataTypeName(op1, false, false));
+    showInfoDialog("Operation to be coded:", errorMessage, NULL, NULL);
+  #endif
+}
+
+
+
+/********************************************//**
+ * \brief regX ==> regL and sin(regX) ==> regX
+ * enables stack lift and refreshes the stack
+ *
+ * \param[in] unusedParamButMandatory uint16_t
+ * \return void
+ ***********************************************/
 void fnSin(uint16_t unusedParamButMandatory) {
-  #if (LOG_FUNCTIONS == 1)
-    enteringFunction("fnSin");
-  #endif
+  if(Sin[getRegisterDataType(REGISTER_X)] != errorSin) {
+    copySourceRegisterToDestRegister(REGISTER_X, REGISTER_L);
 
-  bool_t real16 = getRegisterDataType(REGISTER_X) == dtReal16;
+    result = REGISTER_X;
+    op1    = allocateTemporaryRegister();
+    copySourceRegisterToDestRegister(REGISTER_X, op1);
 
-  copySourceRegisterToDestRegister(REGISTER_X, REGISTER_L);
+    Sin[getRegisterDataType(REGISTER_X)]();
+    freeTemporaryRegister(op1);
 
-  if(getRegisterDataType(REGISTER_X) == dtBigInteger) {
-    convertBigIntegerRegisterToReal34Register(REGISTER_X, REGISTER_X);
-    real16 = true;
+    refreshStack();
   }
-
-  if(getRegisterDataType(REGISTER_X) == dtReal16 || getRegisterDataType(REGISTER_X) == dtReal34) {
-    if(real16) {
-      convertRegister16To34(REGISTER_X);
-    }
-
-    if(real34IsSpecial(POINTER_TO_REGISTER_DATA(REGISTER_X))) {
-      real34Copy(const34_NaN, POINTER_TO_REGISTER_DATA(REGISTER_X));
-    }
-    else {
-    	 WP34S_cvt_2rad_sincos(REAL34_POINTER(POINTER_TO_REGISTER_DATA(REGISTER_X)), NULL, REGISTER_X);
-    	 setRegisterDataType(REGISTER_X, dtReal34);
-    	 setRegisterDataInfo(REGISTER_X, 0);
-    }
-
-    if(real16) {
-      convertRegister34To16(REGISTER_X);
-    }
-
-    roundRegister(REGISTER_X);
-    refreshRegisterLine(REGISTER_X);
-  }
-
   else {
-    displayCalcErrorMessage(24, REGISTER_T, REGISTER_X);
-    #if (EXTRA_INFO_ON_CALC_ERROR == 1)
-      sprintf(errorMessage, "X register is %s", getRegisterDataTypeName(REGISTER_X, true, false));
-      showInfoDialog("In function fnSin:", errorMessage, "is not supported for cos(X)!", NULL);
-    #endif
+    errorSin();
   }
+}
 
-  #if (LOG_FUNCTIONS == 1)
-    leavingFunction("fnSin");
-  #endif
+
+
+void sinBigI(void) {
+  convertBigIntegerRegisterToReal34Register(op1, op1);
+  reallocateRegister(result, dtReal34, REAL34_SIZE, 0);
+  WP34S_cvt_2rad_sincos(REAL34_POINTER(POINTER_TO_REGISTER_DATA(result)), NULL, op1);
+  convertRegister34To16(result);
+}
+
+
+
+void sinRe16(void) {
+  if(real16IsSpecial(POINTER_TO_REGISTER_DATA(op1))) {
+    real16Copy(const16_NaN, POINTER_TO_REGISTER_DATA(result));
+  }
+  else {
+    convertRegister16To34(op1);
+    reallocateRegister(result, dtReal34, REAL34_SIZE, 0);
+    WP34S_cvt_2rad_sincos(REAL34_POINTER(POINTER_TO_REGISTER_DATA(result)), NULL, op1);
+    convertRegister34To16(result);
+  }
+}
+
+
+
+void sinCo16(void) {
+  // sin(z) = (exp(iz) - exp(-iz)) / 2i
+  complex34_t iz, expIz;
+
+  convertRegister16To34(op1);
+  convertRegister16To34(result);
+
+  // calculate iz
+  real34Copy(REGISTER_REAL34_DATA(op1), VARIABLE_IMAG34_DATA(iz));
+  real34Copy(REGISTER_IMAG34_DATA(op1), VARIABLE_REAL34_DATA(iz));
+  real34ChangeSign(VARIABLE_REAL34_DATA(iz));
+
+  // calculate exp(iz)
+  complex34Copy(VARIABLE_COMPLEX34_DATA(iz), REGISTER_COMPLEX34_DATA(op1));
+  expCo34();
+  complex34Copy(REGISTER_COMPLEX34_DATA(result), VARIABLE_COMPLEX34_DATA(expIz));
+
+  // calculate exp(-iz)
+  complex34ChangeSign(VARIABLE_COMPLEX34_DATA(iz));
+  complex34Copy(VARIABLE_COMPLEX34_DATA(iz), REGISTER_COMPLEX34_DATA(op1));
+  expCo34();
+
+  // calculate exp(iz) - exp(-iz)
+  op2 = allocateTemporaryRegister();
+  reallocateRegister(op2, dtComplex34, COMPLEX34_SIZE, 0);
+  complex34Copy(VARIABLE_COMPLEX34_DATA(expIz),  REGISTER_COMPLEX34_DATA(op1));
+  complex34Copy(REGISTER_COMPLEX34_DATA(result), REGISTER_COMPLEX34_DATA(op2));
+  subCo34Co34();
+
+  // calculate (exp(iz) - exp(-iz)) / 2i
+  complex34Copy(REGISTER_COMPLEX34_DATA(result), REGISTER_COMPLEX34_DATA(op1));
+  real34Zero(REGISTER_REAL34_DATA(op2));
+  real34Copy(const34_2, REGISTER_IMAG34_DATA(op2));
+  divCo34Co34();
+
+  freeTemporaryRegister(op2);
+  convertRegister34To16(result);
+}
+
+
+
+void sinRm16(void) {
+  sinToBeCoded();
+}
+
+
+
+void sinCm16(void) {
+  sinToBeCoded();
+}
+
+
+
+void sinRe34(void) {
+  if(real34IsSpecial(POINTER_TO_REGISTER_DATA(op1))) {
+    real34Copy(const34_NaN, POINTER_TO_REGISTER_DATA(result));
+  }
+  else {
+    WP34S_cvt_2rad_sincos(REAL34_POINTER(POINTER_TO_REGISTER_DATA(result)), NULL, op1);
+  }
+}
+
+
+
+void sinCo34(void) {
+  // sin(z) = (exp(iz) - exp(-iz)) / 2i
+  complex34_t iz, expIz;
+
+  // calculate iz
+  real34Copy(REGISTER_REAL34_DATA(op1), VARIABLE_IMAG34_DATA(iz));
+  real34Copy(REGISTER_IMAG34_DATA(op1), VARIABLE_REAL34_DATA(iz));
+  real34ChangeSign(VARIABLE_REAL34_DATA(iz));
+
+  // calculate exp(iz)
+  complex34Copy(VARIABLE_COMPLEX34_DATA(iz), REGISTER_COMPLEX34_DATA(op1));
+  expCo34();
+  complex34Copy(REGISTER_COMPLEX34_DATA(result), VARIABLE_COMPLEX34_DATA(expIz));
+
+  // calculate exp(-iz)
+  complex34ChangeSign(VARIABLE_COMPLEX34_DATA(iz));
+  complex34Copy(VARIABLE_COMPLEX34_DATA(iz), REGISTER_COMPLEX34_DATA(op1));
+  expCo34();
+
+  // calculate exp(iz) - exp(-iz)
+  op2 = allocateTemporaryRegister();
+  reallocateRegister(op2, dtComplex34, COMPLEX34_SIZE, 0);
+  complex34Copy(VARIABLE_COMPLEX34_DATA(expIz),  REGISTER_COMPLEX34_DATA(op1));
+  complex34Copy(REGISTER_COMPLEX34_DATA(result), REGISTER_COMPLEX34_DATA(op2));
+  subCo34Co34();
+
+  // calculate (exp(iz) - exp(-iz)) / 2i
+  complex34Copy(REGISTER_COMPLEX34_DATA(result), REGISTER_COMPLEX34_DATA(op1));
+  real34Zero(REGISTER_REAL34_DATA(op2));
+  real34Copy(const34_2, REGISTER_IMAG34_DATA(op2));
+  divCo34Co34();
+
+  freeTemporaryRegister(op2);
 }

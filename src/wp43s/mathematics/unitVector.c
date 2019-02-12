ac7b0ea55ee87290613516a948037823b04435b1
Over_score <martin@lorang.net>
2019-02-12 19:53:09 +0000

    Resolve "sign should return an infinite integer instead of a real in most cases (cf. the ReM)."

--- src/wp43s/mathematics/unitVector.c
+++ src/wp43s/mathematics/unitVector.c
@@ -22,47 +22,75 @@
 
 
 
+void (* const unitVector[12])(void) = {
+//  regX ==> 1                2                3               4                5                6                7                8                9                10               11               12
+//           Big integer      real16           complex16       Date             Time             Date             String           real16 mat       complex16 m      Small integer    real34           complex34
+             errorUnitVector, errorUnitVector, unitVectorCo16, errorUnitVector, errorUnitVector, errorUnitVector, errorUnitVector, errorUnitVector, errorUnitVector, errorUnitVector, errorUnitVector, unitVectorCo34
+};
+
+
+
 /********************************************//**
- * \brief Unit vector of a complex or a matrix
+ * \brief Data type error in unitVector
+ *
+ * \param void
+ * \return void
+ ***********************************************/
+void errorUnitVector(void) {
+  displayCalcErrorMessage(24, REGISTER_T, REGISTER_X);
+  #if (EXTRA_INFO_ON_CALC_ERROR == 1)
+    sprintf(errorMessage, "cannot calculate the unit vector of %s", getRegisterDataTypeName(REGISTER_X, true, false));
+    showInfoDialog("In function fnUnitVector:", errorMessage, NULL, NULL);
+  #endif
+}
+
+
+
+/********************************************//**
+ * \brief regX ==> regL and unitVector(regX) ==> regX
+ * enables stack lift and refreshes the stack
  *
  * \param[in] unusedParamButMandatory uint16_t
  * \return void
  ***********************************************/
 void fnUnitVector(uint16_t unusedParamButMandatory) {
-  if(getRegisterDataType(REGISTER_X) == dtComplex16) {
-    real16_t temp;
-
+  if(unitVector[getRegisterDataType(REGISTER_X)] != errorSign) {
     copySourceRegisterToDestRegister(REGISTER_X, REGISTER_L);
-    real16Multiply(REGISTER_REAL16_DATA(REGISTER_X), REGISTER_REAL16_DATA(REGISTER_X), &temp);
-    real16FMA(REGISTER_IMAG16_DATA(REGISTER_X), REGISTER_IMAG16_DATA(REGISTER_X), &temp, &temp);
-    real16SquareRoot(&temp, &temp);
-    real16Divide(REGISTER_REAL16_DATA(REGISTER_X), &temp, REGISTER_REAL16_DATA(REGISTER_X));
-    real16Divide(REGISTER_IMAG16_DATA(REGISTER_X), &temp, REGISTER_IMAG16_DATA(REGISTER_X));
+
+    result = REGISTER_X;
+    opX    = allocateTemporaryRegister();
+    copySourceRegisterToDestRegister(REGISTER_X, opX);
+
+    unitVector[getRegisterDataType(REGISTER_X)]();
+    freeTemporaryRegister(opX);
 
     refreshStack();
   }
-  else if(getRegisterDataType(REGISTER_X) == dtComplex34) {
-    real34_t temp;
-
-    copySourceRegisterToDestRegister(REGISTER_X, REGISTER_L);
-    real34Multiply(REGISTER_REAL34_DATA(REGISTER_X), REGISTER_REAL34_DATA(REGISTER_X), &temp);
-    real34FMA(REGISTER_IMAG34_DATA(REGISTER_X), REGISTER_IMAG34_DATA(REGISTER_X), &temp, &temp);
-    real34SquareRoot(&temp, &temp);
-    real34Divide(REGISTER_REAL34_DATA(REGISTER_X), &temp, REGISTER_REAL34_DATA(REGISTER_X));
-    real34Divide(REGISTER_IMAG34_DATA(REGISTER_X), &temp, REGISTER_IMAG34_DATA(REGISTER_X));
-
-    refreshStack();
-  }
-  #ifdef PC_BUILD
-  else if(getRegisterDataType(REGISTER_X) == dtReal16Matrix || getRegisterDataType(REGISTER_X) == dtComplex16Matrix) {
-    showInfoDialog("In function fnUnitVector:", "UNITV for matrix:", "to be coded", NULL);
-  }
-  #endif
   else {
-    displayCalcErrorMessage(24, REGISTER_T, REGISTER_X);
-    #if (EXTRA_INFO_ON_CALC_ERROR == 1)
-      sprintf(errorMessage, "UNITV doesn't work an %s!", getRegisterDataTypeName(REGISTER_X, true, false));
-      showInfoDialog("In function fnUnitVector:", errorMessage, NULL, NULL);
-    #endif
+    errorSign();
   }
+}
+
+
+
+void unitVectorCo16(void) {
+  real16_t temp;
+
+  real16Multiply(REGISTER_REAL16_DATA(opX), REGISTER_REAL16_DATA(opX), &temp);
+  real16FMA(REGISTER_IMAG16_DATA(opX), REGISTER_IMAG16_DATA(opX), &temp, &temp);
+  real16SquareRoot(&temp, &temp);
+  real16Divide(REGISTER_REAL16_DATA(opX), &temp, REGISTER_REAL16_DATA(result));
+  real16Divide(REGISTER_IMAG16_DATA(opX), &temp, REGISTER_IMAG16_DATA(result));
+}
+
+
+
+void unitVectorCo34(void) {
+  real34_t temp;
+
+  real34Multiply(REGISTER_REAL34_DATA(opX), REGISTER_REAL34_DATA(opX), &temp);
+  real34FMA(REGISTER_IMAG34_DATA(opX), REGISTER_IMAG34_DATA(opX), &temp, &temp);
+  real34SquareRoot(&temp, &temp);
+  real34Divide(REGISTER_REAL34_DATA(opX), &temp, REGISTER_REAL34_DATA(result));
+  real34Divide(REGISTER_IMAG34_DATA(opX), &temp, REGISTER_IMAG34_DATA(result));
 }

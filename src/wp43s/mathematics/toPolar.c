b4a9e96da4e12bca98b75effaff9a39e8a57ab1c
Over_score <martin@lorang.net>
2019-08-01 17:23:40 +0000

    Update toPolar.c

--- src/wp43s/mathematics/toPolar.c
+++ src/wp43s/mathematics/toPolar.c
@@ -96,7 +96,6 @@ void fnToPolar(uint16_t unusedParamButMandatory) {
     }
 
     temporaryInformation = TI_RADIUS_THETA;
-printf("temporaryInformation = TI_RADIUS_THETA\n");
     refreshRegisterLine(REGISTER_X);
     refreshRegisterLine(REGISTER_Y);
   }

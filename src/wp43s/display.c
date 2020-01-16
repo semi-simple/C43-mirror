1295545c4e792361417df862282ef9532a3f0220
Jaco Mostert <jaco@cocoon-creations.com>
2020-01-16 13:04:16 +0200

    Line up longint display

--- src/wp43s/display.c
+++ src/wp43s/display.c
@@ -1584,12 +1584,16 @@ void shortIntegerToDisplayString(calcRegister_t regist, char *displayString, con
 
 
 
-void longIntegerToDisplayString(calcRegister_t regist, char *displayString, int32_t strLg, int16_t maxWidth, int16_t maxExp) {
+void longIntegerToDisplayString(calcRegister_t regist, char *displayString, int32_t strLg, int16_t maxWid, int16_t maxExp) {
   int16_t len, exponentStep;
   uint32_t exponentShift, exponentShiftLimit;
   longInteger_t lgInt;
+  int16_t maxWidth;                                          //JM align longints
 
   convertLongIntegerRegisterToLongInteger(regist, lgInt);
+
+  if(longIntegerIsNegative(lgInt)) {maxWidth = maxWid;}      //JM align longints
+  else {maxWidth = maxWid - 8;}                              //JM align longints
 
   exponentShift = (longIntegerBits(lgInt) - 1) * 0.3010299956639811952137;
   exponentStep = (groupingGap == 0 ? 1 : groupingGap);
@@ -1733,9 +1737,7 @@ void longIntegerToAllocatedString(longInteger_t lgInt, char *str, int32_t strLen
     str[0] = '-';
   }
   else {
-    stringLen = digits + 1 ;//+2; // 1 for the trailing 0 //JM add space
-    //str[0] = 0xa0;             //JM add space 218a   a1 8a     batt:242a a4 2a
-    //str[1] = 0x08;             //JM add space
+    stringLen = digits + 1; // 1 for the trailing 0
   }
 
   if(strLen < stringLen) {

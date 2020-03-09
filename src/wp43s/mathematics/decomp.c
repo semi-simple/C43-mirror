969c50f20843323d4aeec4c5db7dd3cb093aafe7
JohnBoydon <john.boydon@gmail.com>
2020-03-09 18:47:41 +0100

    DECOMP implementes and tested.

--- src/wp43s/mathematics/decomp.c
+++ src/wp43s/mathematics/decomp.c
@@ -20,15 +20,23 @@
 
 #include "wp43s.h"
 
-static void (* const Decomp[9])(void) = {
+static void (*const Decomp[9])(void) = {
 // regX ==> 1            2           3            4            5            6            7            8            9
 //          Long integer Real34      Complex34    Time         Date         String       Real34 mat   Complex34 m  Short integer
             decompError, decompReal, decompError, decompError, decompError, decompError, decompError, decompError, decompError
 };
 
 
-void fnDecomp(uint16_t unusedParamButMandatory)
-{
+/********************************************//**
+ * \brief regX ==> regL and DECOMP(regX) ==> regX, regY
+ * enables stack lift and refreshes the stack.
+ * Decomposes x (after converting it to an improper fraction, if applicable), returning a stack with
+ * [denominator(x), numerator(x)]
+ *
+ * \param[in] unusedParamButMandatory uint16_t
+ * \return void
+ ***********************************************/
+void fnDecomp(uint16_t unusedParamButMandatory) {
     saveStack();
     copySourceRegisterToDestRegister(REGISTER_X, REGISTER_L);
 
@@ -38,61 +46,54 @@ void fnDecomp(uint16_t unusedParamButMandatory)
     adjustResult(REGISTER_Y, false, true, REGISTER_Y, -1, -1);
 }
 
-
-void decompError(void)
-{
+void decompError(void) {
     displayCalcErrorMessage(ERROR_INVALID_DATA_INPUT_FOR_OP, ERR_REGISTER_LINE, REGISTER_X);
+
 #if (EXTRA_INFO_ON_CALC_ERROR == 1)
     sprintf(errorMessage, "cannot calculate Decomp for %s", getRegisterDataTypeName(REGISTER_X, true, false));
     showInfoDialog("In function fnDecomp:", errorMessage, NULL, NULL);
 #endif
 }
 
-
-static void realMod(real39_t *a, real39_t *b, real39_t *result)
-{
+/* This function calculates the rest of  division of two real numbers.
+ * result = MOD(a, b) = a % b
+ */
+static void modReal(real39_t *a, real39_t *b, real39_t *result) {
     WP34S_Mod(a, b, result);
 
-    if(!realIsZero(result) && realSign(b) != realSign(a))
+    if (!realIsZero(result) && realSign(b) != realSign(a))
         realAdd(result, a, result, &ctxtReal39);
 }
 
-
-static void realGCD(real39_t *x, real39_t *y, real39_t *result)
-{
+/* This funtion calculates the GCD of two real numbers.
+ * result = GCD(x, y)
+ */
+static void gcdReal(real39_t *x, real39_t *y, real39_t *result) {
     real39_t b, t;
 
     realCopy(y, &b);                // b = y
     realCopy(x, result);            // result = x
 
-    while(! realIsZero(&b))         // b != 0
-    {
+    while (!realIsZero(&b)) {       // b != 0
         realCopy(&b, &t);           // t = b;
-        realMod(result, &t, &b);    // b = result mod t
+        modReal(result, &t, &b);    // b = result mod t
         realCopy(&t, result);       // result = t
     }
 }
 
-
 #define MAX_EXPANSION_ITERATIONS     1000
 
-void decompReal(void)
-{
+void decompReal(void) {
     real39_t x;
     real34ToReal(REGISTER_REAL34_DATA(REGISTER_X), &x);
 
-    if(realIsNaN(&x))                                                       // x == NaN
-    {
-        realToReal34(const_NaN,  REGISTER_REAL34_DATA(REGISTER_X));         // Denominator = NaN
-        realToReal34(const_NaN,  REGISTER_REAL34_DATA(REGISTER_Y));         // Numerator = NaN
-    }
-    else if(realIsInfinite(&x))                                             // x == Inf
-    {
-        realToReal34(const_0,  REGISTER_REAL34_DATA(REGISTER_X));           // Denominator = 0
-        realToReal34(realIsNegative(&x) ? const__1 : const_1,  REGISTER_REAL34_DATA(REGISTER_Y)); // Numerator = +/- 1
-    }
-    else
-    {
+    if (realIsNaN(&x)) {                                                    // x == NaN
+        realToReal34(const_NaN, REGISTER_REAL34_DATA(REGISTER_X));          // Denominator = NaN
+        realToReal34(const_NaN, REGISTER_REAL34_DATA(REGISTER_Y));          // Numerator = NaN
+    } else if (realIsInfinite(&x)) {                                        // x == Inf
+        realToReal34(const_0, REGISTER_REAL34_DATA(REGISTER_X));            // Denominator = 0
+        realToReal34(realIsNegative(&x) ? const__1 : const_1, REGISTER_REAL34_DATA(REGISTER_Y)); // Numerator = +/- 1
+    } else {
         real39_t d;                                                         // Denominator
         real39_t n;                                                         // Numerator
         real39_t maxD, z, dOld, t, s;
@@ -100,19 +101,17 @@ void decompReal(void)
         int32ToReal(denMax, &maxD);                                         // maxD = (Real)denMax
         realCopyAbs(&x, &z);                                                // z = abs(x)
 
-        if(denominatorMode == DM_ANY)
-        {
+        if (denominatorMode == DM_ANY) {
             realCopy(const_0, &dOld);                                       // dOld = 0
             realCopy(const_1, &d);                                          // d = 1
             /*
              * Do a partial fraction expansion until the denominator is too large
              */
-            for(int32_t i=0 ; i<MAX_EXPANSION_ITERATIONS ; i++)
-            {
+            for (int32_t i = 0; i < MAX_EXPANSION_ITERATIONS; i++) {
                 realToIntegralValue(&z, &t, DEC_ROUND_DOWN, &ctxtReal39);   // t = trunc(z)
                 realSubtract(&z, &t, &s, &ctxtReal39);                      // s = z - t
 
-                if(realIsZero(&s))                                          // s == 0
+                if (realIsZero(&s))                                         // s == 0
                     break;
 
                 realDivide(const_1, &s, &z, &ctxtReal39);                   // z = 1/s
@@ -120,26 +119,23 @@ void decompReal(void)
                 realMultiply(&s, &d, &t, &ctxtReal39);                      // t = s * d
                 realAdd(&t, &dOld, &s, &ctxtReal39);                        // s = t + dOld (s is new denominator estimate)
 
-                if(realCompareLessThan(&maxD, &s))                          // maxD < s
+                if (realCompareLessThan(&maxD, &s))                         // maxD < s
                     break;
 
                 realCopy(&d, &dOld);                                        // dOld = d
                 realCopy(&s, &d);                                           // d = s
             }
-        }
-        else
+        } else
             realCopy(&maxD, &d);                                            // d = maxD
 
         realMultiply(&x, &d, &t, &ctxtReal39);                              // t = x * d
         realToIntegralValue(&t, &n, DEC_ROUND_HALF_UP, &ctxtReal39);        // n = round(t)
 
-        if(denominatorMode==DM_FAC)
-        {
-            if(realIsZero(&n))                                              // n == 0
+        if (denominatorMode == DM_FAC) {
+            if (realIsZero(&n)) {                                           // n == 0
                 realCopy(const_1, &d);                                      // d = 1
-            else
-            {
-                realGCD(&n, &d, &t);                                        // t = gcd(n, d)
+            } else {
+                gcdReal(&n, &d, &t);                                        // t = gcd(n, d)
                 realDivide(&n, &t, &n, &ctxtReal39);                        // n = n/t
                 realDivide(&d, &t, &d, &ctxtReal39);                        // d = d/t
             }

3ba9518078bdcf1beccf638f91fb2f35a7b6b27d
Over_score <martin@lorang.net>
2020-07-05 14:01:26 +0000

    Changed features 2020 07 05

--- src/wp43s/stats.h
+++ src/wp43s/stats.h
@@ -20,8 +20,10 @@
 
 
 
-void fnSigma                     (uint16_t plusMinus);
-void fnStatSum                   (uint16_t sum);
-void fnSumXY                     (uint16_t unusedParamButMandatory);
-int  checkMinimumDataPoints      (const real_t *n);
-void initStatisticalSums         (void);
+void   fnSigma               (uint16_t plusMinus);
+void   fnStatSum             (uint16_t sum);
+void   fnSumXY               (uint16_t unusedParamButMandatory);
+void   fnXmin                (uint16_t unusedParamButMandatory);
+void   fnXmax                (uint16_t unusedParamButMandatory);
+bool_t checkMinimumDataPoints(const real_t *n);
+void   initStatisticalSums   (void);

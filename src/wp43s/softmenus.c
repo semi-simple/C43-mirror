c20860929afd264fc97f3f6a7c327dc4fafaba0c
Jaco Mostert <jaco@cocoon-creations.com>
2020-10-30 18:09:05 +0200

    Integrate Dani's ifdef on keycount. Change plot menu allocation

--- src/wp43s/softmenus.c
+++ src/wp43s/softmenus.c
@@ -629,12 +629,12 @@ const int16_t menu_GRAPH[]        = {
 const int16_t menu_PLOT[]        = {
           /*-1------*/                                                                                                                                                                                            //JM ALPHA
                                      ITM_PLOT,                      ITM_PLOTLS,                 ITM_SCALE,                ITM_PLINE,             ITM_SIGMAPLUS,               ITM_SCRDMP,                         //JM GRAPH
-                                     ITM_NVECT,                     ITM_VECT,                   ITM_PCROS,                ITM_DIFF,              ITM_SIGMAMINUS,              ITM_LISTXY,                         //JM GRAPH
-                                     ITM_EXTX,                      ITM_EXTY,                   ITM_PBOX,                 ITM_INTG,              ITM_CLSIGMA,                 ITM_SUM,                            //JM GRAPH
+                                     ITM_NVECT,                     ITM_VECT,                   ITM_EXTX,                 ITM_PCROS,             ITM_SIGMAMINUS,              ITM_LISTXY,                         //JM GRAPH
+                                     ITM_DIFF,                      ITM_INTG,                   ITM_EXTY,                 ITM_PBOX,              ITM_CLSIGMA,                 ITM_SUM,                            //JM GRAPH
 
                                      ITM_SCALE,                     ITM_PLINE,                  ITM_NULL,                 ITM_NULL,              ITM_NULL,                    ITM_NULL,                           //JM GRAPH
-                                     ITM_PCROS,                     ITM_DIFF,                   ITM_NULL,                 ITM_NULL,              ITM_NULL,                    ITM_NULL,                           //JM GRAPH
-                                     ITM_PBOX,                      ITM_INTG,                   ITM_NULL,                 ITM_NULL,              ITM_NULL,                    ITM_NULL,                           //JM GRAPH
+                                     ITM_EXTX,                      ITM_PCROS,                  ITM_NULL,                 ITM_NULL,              ITM_NULL,                    ITM_NULL,                           //JM GRAPH
+                                     ITM_EXTY,                      ITM_PBOX,                   ITM_NULL,                 ITM_NULL,              ITM_NULL,                    ITM_NULL,                           //JM GRAPH
 
                                      ITM_SIGMAPLUS,                 ITM_SCRDMP,                 ITM_NULL,                 ITM_NULL,              ITM_NULL,                    ITM_NULL,                           //JM GRAPH
                                      ITM_SIGMAMINUS,                ITM_LISTXY,                 ITM_NULL,                 ITM_NULL,              ITM_NULL,                    ITM_NULL,                           //JM GRAPH

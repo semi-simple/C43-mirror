7802b6aebd0f55f292b61e100d25f9f630068f80
MihailJP <mihailjp@gmail.com>
2021-01-29 20:56:23 +0900

    Implement `INT?`, `EVEN?` and `ODD?`

---
+++ src/wp43s/mathematics/int.c
@@ -0,0 +1,82 @@
+/* This file is part of 43S.
+ *
+ * 43S is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * 43S is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with 43S.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/********************************************//**
+ * \file int.c
+ ***********************************************/
+
+#include "wp43s.h"
+
+
+void checkInteger(enum checkIntegerMode mode) {
+  longInteger_t x, x2;
+
+  if(getRegisterDataType(REGISTER_X) == dtShortInteger) {
+    convertShortIntegerRegisterToLongInteger(REGISTER_X, x);
+    convertShortIntegerRegisterToLongInteger(REGISTER_X, x2);
+  }
+
+  else if(getRegisterDataType(REGISTER_X) == dtLongInteger) {
+    convertLongIntegerRegisterToLongInteger(REGISTER_X, x);
+    convertLongIntegerRegisterToLongInteger(REGISTER_X, x2);
+  }
+
+  else if(getRegisterDataType(REGISTER_X) == dtReal34) {
+    // if ceil(x) != floor(x), then x is not an integer
+    convertReal34ToLongInteger(REGISTER_REAL34_DATA(REGISTER_X), x, DEC_ROUND_CEILING);
+    convertReal34ToLongInteger(REGISTER_REAL34_DATA(REGISTER_X), x2, DEC_ROUND_FLOOR);
+  }
+
+  else {
+    displayCalcErrorMessage(ERROR_INVALID_DATA_TYPE_FOR_OP, ERR_REGISTER_LINE, REGISTER_X);
+    #if(EXTRA_INFO_ON_CALC_ERROR == 1)
+      sprintf(errorMessage, "the input type %s cannot convert to integer", getDataTypeName(getRegisterDataType(REGISTER_X), false, false));
+      moreInfoOnError("In function fnIsOdd:", errorMessage, NULL, NULL);
+    #endif // (EXTRA_INFO_ON_CALC_ERROR == 1)
+  }
+
+  hourGlassIconEnabled = true;
+  showHideHourGlass();
+  #ifdef DMCP_BUILD
+    lcd_refresh();
+  #else // !DMCP_BUILD
+    refreshLcd(NULL);
+  #endif // DMCP_BUILD
+  if(longIntegerCompare(x, x2) == 0) {
+    switch(mode) {
+      case CHECK_INTEGER:
+        temporaryInformation = TI_TRUE;
+        break;
+
+      case CHECK_INTEGER_EVEN:
+        temporaryInformation = (longIntegerIsEven(x) ? TI_TRUE : TI_FALSE);
+        break;
+
+      case CHECK_INTEGER_ODD:
+        temporaryInformation = (longIntegerIsOdd(x) ? TI_TRUE : TI_FALSE);
+        break;
+    }
+  }
+  else {
+    temporaryInformation = TI_FALSE;
+  }
+  longIntegerFree(x);
+  longIntegerFree(x2);
+}
+
+void fnIsInt(uint16_t unusedButMandatoryParameter) {
+  checkInteger(CHECK_INTEGER);
+}

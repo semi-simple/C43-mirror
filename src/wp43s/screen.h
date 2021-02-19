2ecd9c71ad933acad2c65af7d0661fa6180cade0
Over_score <martin@lorang.net>
2021-02-19 11:30:03 +0100

    Cosmetic changes

--- src/wp43s/screen.h
+++ src/wp43s/screen.h
@@ -34,7 +34,7 @@ void       fnScreenDump                       (uint16_t unusedButMandatoryParame
 #ifdef DMCP_BUILD
   void     refreshLcd                         (void);
 #else // !DMCP_BUILD
-  void     lcd_fill_rect                      (uint32_t x, uint32_t y, uint32_t dx, uint32_t  dy, int val); // clone fron the DMCP function
+  void     lcd_fill_rect                      (uint32_t x, uint32_t y, uint32_t dx, uint32_t dy, int val); // clone fron the DMCP function
   void     setBlackPixel                      (uint32_t x, uint32_t y);
   void     setWhitePixel                      (uint32_t x, uint32_t y);
 #endif // DMCP_BUILD

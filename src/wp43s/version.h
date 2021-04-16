2b8c9799a432c9186ff6c4dcd3d5cc9225189e89
Jaco Mostert <jaco@cocoon-creations.com>
2021-04-16 02:58:35 +0200

    Add binaries

--- src/wp43s/version.h
+++ src/wp43s/version.h
@@ -25,7 +25,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION_STRING "custom" STD_SPACE_3_PER_EM "build" STD_SPACE_3_PER_EM "369418c0-mod"
-#define VERSION_SHORT  "369418c0-mod"
+#define VERSION_STRING "custom" STD_SPACE_3_PER_EM "build" STD_SPACE_3_PER_EM "3c6e5f2e-mod"
+#define VERSION_SHORT  "3c6e5f2e-mod"
 
 #endif // VERSION_H

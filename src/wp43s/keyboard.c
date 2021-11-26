0653c48e42bf924f4fdad42e1fdf1464f0591c07
Jaco Mostert <jaco@cocoon-creations.com>
2021-11-26 22:31:21 +0200

    comment fix

--- src/wp43s/keyboard.c
+++ src/wp43s/keyboard.c
@@ -784,7 +784,6 @@ bool_t lowercaseselected;
     /*2*/ else if(lowercaseselected && ((ITM_ALPHA <= item && item <= ITM_OMEGA) || (ITM_QOPPA <= item && item <= ITM_SAMPI))) {  //JM GREEK
       addItemToBuffer(item); 
       //addItemToBuffer(item + 36); //JM Remove the ability to shift to lower cap greek
-  //Changed this. Still painting Delta in lower cae.    
       keyActionProcessed = true;
     }
 

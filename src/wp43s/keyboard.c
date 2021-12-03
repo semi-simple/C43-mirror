93cef5f0260db61f0c0dc3f8b3287b4cc48c1820
MihailJP <mihailjp@gmail.com>
2021-12-04 01:54:05 +0900

    Fix GTO . PROG

--- src/wp43s/keyboard.c
+++ src/wp43s/keyboard.c
@@ -75,6 +75,15 @@
         break;
 
       case MNU_PROG:
+        dynamicMenuItem = firstItem + itemShift + (fn - 1);
+        if(tam.function == ITM_GTOP) {
+          item = ITM_GTOP;
+        }
+        else {
+          item = (dynamicMenuItem >= dynamicSoftmenu[menuId].numItems ? ITM_NOP : MNU_DYNAMIC);
+        }
+        break;
+
       case MNU_VAR:
         dynamicMenuItem = firstItem + itemShift + (fn - 1);
         item = (dynamicMenuItem >= dynamicSoftmenu[menuId].numItems ? ITM_NOP : MNU_DYNAMIC);
@@ -374,6 +383,10 @@
           }
           else if(calcMode == CM_PEM && catalog && catalog != CATALOG_MVAR) { // TODO: is that correct
             runFunction(item);
+            if(tam.function == ITM_GTOP) {
+              tamLeaveMode();
+              hourGlassIconEnabled = false;
+            }
             refreshScreen();
             return;
           }
